ab6bf99d6b67a39d2d2c32c0b4ac8e2e6a8632b0
joesurf <82246537+joesurf@users.noreply.github.com>
2022-05-25 17:26:36 +0800

    Delete djangomain directory

--- djangomain/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ActivityView
-
-urlpatterns = [
-    path('activity/', ActivityView.as_view()),
-]
--- djangomain/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import ActivitySerializer
-from .models import Activity
-
-# Create your views here.
-class ActivityView(generics.ListAPIView):
-    queryset = Activity.objects.all()
-    serializer_class = ActivitySerializer
