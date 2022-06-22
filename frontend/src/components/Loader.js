852c27bd1ac88147334342653560b687aab15372
joesurf <82246537+joesurf@users.noreply.github.com>
2022-06-22 17:42:17 +0800

    changed loader animation to movie

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Spinner } from 'react-bootstrap';
 
+import './loader.css';
+
 function Loader() {
   return (
-    <Spinner 
-      animation='border' 
-      role='status' 
-      style={{
-        height: '100px',
-        width: '100px',
-        margin: 'auto',
-        display: 'block'
-      }}
-    >
-      <span className='sr-only'>Loading...</span>
-    </Spinner>
+    <div className="loader-main">
+      <div className="loader">
+        <div className="loader__filmstrip">
+        </div>
+        <p className="loader__text">
+          loading
+        </p>
+      </div>
+    </div>
+
+
+//     <Spinner 
+//       animation='border' 
+//       role='status' 
+//       style={{
+//         height: '100px',
+//         width: '100px',
+//         margin: 'auto',
+//         display: 'block'
+//       }}
+//     >
+//       <span className='sr-only'>Loading...</span>
+//     </Spinner>
   )
 }
 
